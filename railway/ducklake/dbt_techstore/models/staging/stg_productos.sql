e0a5426ba8e2e57f46b0264f97f4bd77461928bf
@oscar6955739 <oscar@theblackpuma.com>
2026-02-15 21:42:58 -0300

    Update stg_productos.sql

--- railway/ducklake/dbt_techstore/models/staging/stg_productos.sql
+++ railway/ducklake/dbt_techstore/models/staging/stg_productos.sql
@@ -4,3 +4,4 @@ select
     costo, codigo_proveedor,
     round((precio_unitario - costo) * 100.0 / precio_unitario, 1) as margen_pct
 from {{ source('raw', 'productos') }}
+where categoria is not null
